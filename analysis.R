e20bcc2c9522301585e85a09c390d0838685e9fb
Berto-Seoul <68474796+Berto-Seoul@users.noreply.github.com>
2021-07-28 11:26:56 +0900

    write down the goals

--- analysis.R
+++ analysis.R
@@ -1,3 +1,14 @@
 pacman::p_load(dplyr,stringr,writexl,readxl,stringi,naniar,tidyverse)
 
-Hey meat this thing on this one
+load("p.fre.meat.Rdata")
+
+meat <- p.fre.meat
+
+names(meat)
+str(meat)
+
+# 목표
+# 1) 국내 쇠고기 구매패턴을 분석
+# 2) 이후 주요 육류(소, 닭, 돼지) 별, 부위(갈비, 등심 별)
+# 3) 구매채널(예: 오프라인, 온라인) 별 구체적인 소비트렌드 제시
+
